5b57a367aee55fe70ca0723b10f378f72cecffa5
chloepei867 <pei.zhiy@northeastern.edu>
2025-09-18 14:17:04 -0700

    fixed the scroll effect

--- src/ReviewPage.jsx
+++ src/ReviewPage.jsx
@@ -4,13 +4,13 @@ import ReviewSummary from "./components/ReviewSummary";
 export default function ReviewPage() {
   return (
     <div className="flex flex-col justify-center self-stretch grow py-16 rounded bg-neutral-600">
-      <div className="flex flex-col justify-center items-center self-stretch grow bg-white rounded-lg overflow-y-scroll">
+      <div className="flex flex-col justify-center items-center self-stretch grow bg-white rounded-lg">
         <div className="flex flex-col justify-center items-end gap-4 self-stretch p-6">
           <button className="w-6 h-6">
             <i className="fa-solid fa-xmark text-neutral-900 text-lg"></i>
           </button>
         </div>
-        <div className="flex flex-col items-center gap-10 self-stretch grow">
+        <div className="flex flex-col items-center gap-10 self-stretch max-h-[70vh] overflow-y-auto">
           <ReviewSummary />
           <ReviewList />
         </div>
