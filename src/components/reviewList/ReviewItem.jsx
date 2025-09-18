e8c601e81663d4768ae2c0a92d7ce080cc2ebd30
chloepei867 <pei.zhiy@northeastern.edu>
2025-09-18 15:46:20 -0700

    created reviewList component

--- src/components/reviewList/ReviewItem.jsx
+++ src/components/reviewList/ReviewItem.jsx
@@ -1,6 +1,14 @@
 import StarRating from "../StarRating";
 
-export default function ReviewItem() {
+export default function ReviewItem({ review }) {
+  const formatted = (dateStr) => {
+    return new Date(dateStr).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short", // full month name
+      day: "numeric",
+    });
+  };
+
   return (
     <div className="flex flex-col gap-4 self-stretch">
       {/* avatar details */}
@@ -8,9 +16,9 @@ export default function ReviewItem() {
         {/* avatar */}
         <div className="flex w-12 h-12 rounded-full overflow-hidden">
           <img
-            className="object-cover"
-            src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/e-commerce-track-images/user-avatars/kimberly-mastrangelo.jpg"
-            alt=""
+            className="flex w-full h-full object-cover"
+            src={review.userAvatar}
+            alt={review.userName}
           />
         </div>
         {/* details */}
@@ -18,22 +26,19 @@ export default function ReviewItem() {
           {/* title */}
           <div className="flex items-center justify-between self-stretch">
             <h3 className="w-[160px] tex-wrap font-semibold text-base text-neutral-900">
-              Kimberly Mastrangelo
+              {review.userName}
             </h3>
-            <span className="flex w-[90px] font-normal text-xs text-neutral-600">
-              March 10, 2024
+            <span className="flex justify-end w-[90px] font-normal text-xs text-neutral-600">
+              {formatted(review.created_at)}
             </span>
           </div>
           <div className="flex items-start">
-            <StarRating value={5} max={5} />
+            <StarRating value={review.rating} max={5} />
           </div>
         </div>
       </div>
       {/* review content */}
-      <p className="font-normal text-base text-neutral-600">
-        Absolutely a must-have for anyone who enjoys outdoor activities. Keeps
-        me warm and looks great!
-      </p>
+      <p className="font-normal text-base text-neutral-600">{review.content}</p>
     </div>
   );
 }
--- src/components/reviewList/ReviewListContainer.jsx
+++ src/components/reviewList/ReviewListContainer.jsx
@@ -1,12 +1,13 @@
 import StarRating from "../StarRating";
 import ReviewItem from "./ReviewItem";
+import reviews from "../../data/mergedReviews.json";
 
 export default function ReviewListContainer() {
   return (
     <div className="flex flex-col justify-center items-end gap-6 self-stretch px-4">
-      <ReviewItem />
-      <ReviewItem />
-      <ReviewItem />
+      {reviews.map((review, index) => (
+        <ReviewItem review={review} />
+      ))}
     </div>
   );
 }
